ae397bcae235e2bbd2c05d47c49525383dc1a4bd
Abdelrahman Omran <me@omranic.com>
2017-08-22 19:50:15 +0200

    Update models docblock

--- src/Models/Page.php
+++ src/Models/Page.php
@@ -15,6 +15,45 @@ use Illuminate\Database\Eloquent\Builder;
 use Rinvex\Support\Traits\UniqueInjector;
 use Spatie\EloquentSortable\SortableTrait;
 
+/**
+ * Rinvex\Pages\Models\Page.
+ *
+ * @property int            $id
+ * @property string         $uri
+ * @property string         $slug
+ * @property string         $domain
+ * @property string         $middleware
+ * @property array          $title
+ * @property array          $subtitle
+ * @property array          $excerpt
+ * @property array          $content
+ * @property string         $view
+ * @property bool           $is_active
+ * @property int            $sort_order
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ * @property \Carbon\Carbon $deleted_at
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|\Rinvex\Pages\Models\Page active()
+ * @method static \Illuminate\Database\Eloquent\Builder|\Rinvex\Pages\Models\Page inactive()
+ * @method static \Illuminate\Database\Eloquent\Builder|\Rinvex\Pages\Models\Page ordered($direction = 'asc')
+ * @method static \Illuminate\Database\Eloquent\Builder|\Rinvex\Pages\Models\Page whereContent($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Rinvex\Pages\Models\Page whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Rinvex\Pages\Models\Page whereDeletedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Rinvex\Pages\Models\Page whereDomain($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Rinvex\Pages\Models\Page whereExcerpt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Rinvex\Pages\Models\Page whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Rinvex\Pages\Models\Page whereIsActive($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Rinvex\Pages\Models\Page whereMiddleware($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Rinvex\Pages\Models\Page whereSlug($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Rinvex\Pages\Models\Page whereSortOrder($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Rinvex\Pages\Models\Page whereSubtitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Rinvex\Pages\Models\Page whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Rinvex\Pages\Models\Page whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Rinvex\Pages\Models\Page whereUri($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Rinvex\Pages\Models\Page whereView($value)
+ * @mixin \Eloquent
+ */
 class Page extends Model implements Sortable
 {
     use HasSlug;

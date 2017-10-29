e7508d0c22db4afc301fb882102f6f380c642a81
Abdelrahman Omran <me@omranic.com>
2017-10-29 05:21:05 +0200

    Tweak artisan command output lines

--- src/Console/Commands/MigrateCommand.php
+++ src/Console/Commands/MigrateCommand.php
@@ -29,7 +29,7 @@ class MigrateCommand extends Command
      */
     public function handle()
     {
-        $this->warn('Migrate rinvex/pages:');
+        $this->warn($this->description);
         $this->call('migrate', ['--step' => true, '--path' => 'vendor/rinvex/pages/database/migrations']);
     }
 }

c9231b9da05953ba97cb9821d981c1181b32c3b8
Abdelrahman Omran <me@omranic.com>
2018-02-19 18:59:59 +0200

    Rename page "slug" to "name"

--- database/migrations/2017_07_30_155655_create_pages_table.php
+++ database/migrations/2017_07_30_155655_create_pages_table.php
@@ -18,7 +18,7 @@ class CreatePagesTable extends Migration
             // Columns
             $table->increments('id');
             $table->string('uri');
-            $table->string('slug');
+            $table->string('name');
             $table->string('route');
             $table->string('domain')->nullable();
             $table->string('middleware')->nullable();
@@ -34,7 +34,7 @@ class CreatePagesTable extends Migration
 
             // Indexes
             $table->unique(['domain', 'uri']);
-            $table->unique(['domain', 'slug']);
+            $table->unique(['domain', 'name']);
             $table->unique(['domain', 'route']);
         });
     }

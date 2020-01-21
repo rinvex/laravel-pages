ed45e034839c03be276c5196843dbf9dd110b292
Abdelrahman Omran <me@omranic.com>
2020-01-21 04:43:35 +0400

    Tweak service provider `publishesResources` & `autoloadMigrations`

--- src/Providers/PagesServiceProvider.php
+++ src/Providers/PagesServiceProvider.php
@@ -61,9 +61,9 @@ class PagesServiceProvider extends ServiceProvider
         $this->loadRoutes($router);
 
         // Publish Resources
-        ! $this->app->runningInConsole() || $this->publishesConfig('rinvex/laravel-pages');
-        ! $this->app->runningInConsole() || $this->publishesMigrations('rinvex/laravel-pages');
-        ! $this->app['config']['rinvex.statistics.autoload_migrations'] || $this->loadMigrationsFrom(__DIR__.'/../../database/migrations');
+        $this->publishesConfig('rinvex/laravel-pages');
+        $this->publishesMigrations('rinvex/laravel-pages');
+        ! $this->autoloadMigrations('rinvex.pages') || $this->loadMigrationsFrom(__DIR__.'/../../database/migrations');
     }
 
     /**

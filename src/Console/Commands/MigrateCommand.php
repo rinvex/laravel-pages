6edd06703234fa9d492361a92338fb5f38c36e09
Abdelrahman Omran <me@omranic.com>
2020-03-18 15:16:32 +0400

    Fix migrations path condition

--- src/Console/Commands/MigrateCommand.php
+++ src/Console/Commands/MigrateCommand.php
@@ -31,7 +31,11 @@ class MigrateCommand extends Command
     {
         $this->alert($this->description);
 
-        if (file_exists($path = 'database/migrations/rinvex/laravel-pages')) {
+        $path = config('rinvex.pages.autoload_migrations') ?
+            'vendor/rinvex/laravel-pages/database/migrations' :
+            'database/migrations/rinvex/laravel-pages';
+
+        if (file_exists($path)) {
             $this->call('migrate', [
                 '--step' => true,
                 '--path' => $path,
--- src/Console/Commands/RollbackCommand.php
+++ src/Console/Commands/RollbackCommand.php
@@ -31,7 +31,11 @@ class RollbackCommand extends Command
     {
         $this->alert($this->description);
 
-        if (file_exists($path = 'database/migrations/rinvex/laravel-pages')) {
+        $path = config('rinvex.pages.autoload_migrations') ?
+            'vendor/rinvex/laravel-pages/database/migrations' :
+            'database/migrations/rinvex/laravel-pages';
+
+        if (file_exists($path)) {
             $this->call('migrate:reset', [
                 '--path' => $path,
                 '--force' => $this->option('force'),

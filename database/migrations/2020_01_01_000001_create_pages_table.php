c964ac12a2e269f43e74143bf4f78517a841320f
Abdelrahman Omran <me@omranic.com>
2020-04-08 02:46:36 +0400

    Reverse commit "Convert database int fields into bigInteger"

--- database/migrations/2020_01_01_000001_create_pages_table.php
+++ database/migrations/2020_01_01_000001_create_pages_table.php
@@ -16,7 +16,7 @@ class CreatePagesTable extends Migration
     {
         Schema::create(config('rinvex.pages.tables.pages'), function (Blueprint $table) {
             // Columns
-            $table->bigIncrements('id');
+            $table->increments('id');
             $table->string('uri');
             $table->string('slug');
             $table->string('route');
